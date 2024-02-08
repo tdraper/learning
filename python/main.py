6b9feda5b4e9d47d24564a112cb5a3f7dac21e8f
Tristan Draper <107201220+tdraper@users.noreply.github.com>
2024-02-08 02:17:56 +0000

    "doing hello world for the folders"

---
+++ python/main.py
@@ -0,0 +1,14 @@
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers)  # Printing original list contents.
+ 
+numbers[0] = 111
+print("\nPrevious list contents:", numbers)  # Printing previous list contents.
+ 
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("New list contents:", numbers)  # Printing current list contents.
+
+print("\nLength of list: ", len(numbers))
+
+del numbers[1:]
+
+print("\nNew Length list:", len(numbers))
