6a8b1c37e6664cbcad30b645ee388b9f02fc5f92
Tristan Draper <107201220+tdraper@users.noreply.github.com>
2024-02-09 18:54:48 +0000

    "cargo build"

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d
@@ -0,0 +1,5 @@
+/workspaces/learning/rust/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499: src/main.rs
+
+/workspaces/learning/rust/hello_cargo/target/debug/deps/hello_cargo-998b8934749f5499.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/workspaces/learning/rust/hello_cargo/target/debug/hello_cargo: /workspaces/learning/rust/hello_cargo/src/main.rs
